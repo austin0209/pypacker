026b833413687945360229bb06c1be566a9c4eaa
austin0209 <austinrandy0209@gmail.com>
2020-07-14 17:26:57 -0500

    added rudimentary packing algorithm and gitignore

--- packer.py
+++ packer.py
@@ -17,31 +17,48 @@ class Canvas():
     def __init__(self, w, h):
         self.width = w
         self.height = h
-        nodes = []
-        nodes.append(CanvasNode(0, 0, w, h))
+        self.nodes = []
+        self.nodes.append(CanvasNode(0, 0, w, h))
+
+    def defragment(self):
+        for n in self.nodes:
+            if (n.get_area() == 0):
+                n.remove(n)
 
     def add_image(self, image):
-        for CanvasNode n in nodes:
-            if n.image == None && n.width >= image.width && n.height >= image.height:
+        for n in self.nodes:
+            if n.image == None and n.width >= image.width and n.height >= image.height:
                 newnode = CanvasNode(n.x, n.y, image.width, image.height, image)
                 left = CanvasNode(n.x + image.width, n.y, n.width - image.width, image.height)
                 bot = CanvasNode(n.x, n.y + image.height, n.width, n.height - image.height)
+                self.nodes.remove(n) # this should be okay bc returning anyways
+                self.nodes.extend([newnode, left, bot])
+                self.defragment()
+                self.nodes.sort(key=lambda a : a.get_area(), reverse=True)
+                return
+        assert(False)
+
+    def get_result(self):
+        result = Image.new("RGBA", (self.width, self.height))
+        for n in self.nodes:
+            if n.image != None:
+                result.paste(n.image, (n.x, n.y))
+        return result
 
 path = sys.argv[1]
 
+canvas = Canvas(1280, 720);
 images = []
-canvas = Image.new("RGB", (1280, 720));
+result = Image.new("RGBA", (1280, 720));
 
 with os.scandir(path) as it:
     for entry in it:
         if entry.is_file:
-            i = Image.open(entry.path))
+            i = Image.open(entry.path)
             images.append(i)
             print("Found image: ", entry.path)
 
-off = 0
-for i in range(0, len(images)):
-    canvas.paste(images[i], (off, 0))
-    off += images[i].width
+for img in sorted(images, key=lambda i : i.width * i.height, reverse=True):
+    canvas.add_image(img)
 
-canvas.save("test.png")
+canvas.get_result().save("result.png")
