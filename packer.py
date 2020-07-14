2a6fae822e7bd7a48074c8386be7ad056613a6da
austin0209 <austinrandy0209@gmail.com>
2020-07-14 13:13:49 -0500

    started structure of program

--- packer.py
+++ packer.py
@@ -2,23 +2,35 @@ import sys
 import os
 from PIL import Image
 
-class VirtualImage():
+class CanvasNode():
+    def __init__(self, x, y, w, h, image=None):
+        self.x = x
+        self.y = y
+        self.width = w
+        self.height = h
+        self.image = image
+
+class Canvas():
     def __init__(self, w, h):
-        self.width = w;
-        self.height = h;
+        self.width = w
+        self.height = h
+        nodes = []
+        nodes.append(CanvasNode(0, 0, w, h))
+
+    def add_image(self, image):
+        
 
 path = sys.argv[1]
 
 images = []
-virt-images = []
 canvas = Image.new("RGB", (1280, 720));
 
 with os.scandir(path) as it:
     for entry in it:
         if entry.is_file:
-            images.append(Image.open(entry.path))
-
-            print(entry.path)
+            i = Image.open(entry.path))
+            images.append(i)
+            print("Found image: ", entry.path)
 
 off = 0
 for i in range(0, len(images)):
