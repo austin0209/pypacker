a44af09f707d8f9d93ed755857b35abda8091f92
Austin Aurelio <austinrandy0209@gmail.com>
2020-07-14 16:13:58 -0500

    some more work

--- packer.py
+++ packer.py
@@ -10,6 +10,9 @@ class CanvasNode():
         self.height = h
         self.image = image
 
+    def get_area(self):
+        return self.width * self.height
+
 class Canvas():
     def __init__(self, w, h):
         self.width = w
@@ -18,7 +21,11 @@ class Canvas():
         nodes.append(CanvasNode(0, 0, w, h))
 
     def add_image(self, image):
-        
+        for CanvasNode n in nodes:
+            if n.image == None && n.width >= image.width && n.height >= image.height:
+                newnode = CanvasNode(n.x, n.y, image.width, image.height, image)
+                left = CanvasNode(n.x + image.width, n.y, n.width - image.width, image.height)
+                bot = CanvasNode(n.x, n.y + image.height, n.width, n.height - image.height)
 
 path = sys.argv[1]
 
