6d13cb398bc6a6f184096eb319c8bf5759ec5206
Austin Aurelio <austinrandy0209@gmail.com>
2020-07-16 21:52:03 -0500

    changed json structure and added recursive file searching

--- packer.py
+++ packer.py
@@ -4,13 +4,13 @@ import json
 from PIL import Image
 
 def print_help():
-    print("Usage: python pypacker.py <PATH_TO_IMAGE_FOLDER> <OPTIONAL PARAMS>\n")
-    print("Availiable parameters:")
-    print("\t--help: open this dialogue")
-    print("\t--output <PATH_TO_DIR>: set folder to output to, must be an existing directory")
-    print("\t--filename <NAME>: set filename of output (ex. filename.png, filename.json)")
-    print("\t--border <INT>: set space between each sprite.")
-    print("\t--notrim: if used, transparent padding will not be trimmed. Can be useful if the script is too slow.")
+    print("usage:  python pypacker.py <path to image folder> [optional params]")
+    print("availiable parameters:")
+    print("\t{-h --help}:\n\t\topen this dialogue")
+    print("\t{-o --output} <path to directory>:\n\t\tset folder to output to, must be an existing directory]\n\t\tdefault: current working directory.")
+    print("\t{-f --filename} <name>:\n\t\tset filename of outputs (filename.png, filename.json). do not include the file extension here.\n\t\tdefault: \"result\".")
+    print("\t{-b --border} <integer value>:\n\t\tset space between each sprite.\n\t\tdefault: 0")
+    print("\t{-nt --notrim}:\n\t\tif used, transparent padding will not be trimmed. can be useful if the script is too slow.")
 
 try:
     path = sys.argv[1]
@@ -18,10 +18,11 @@ except:
     print_help()
     exit()
 outpath = ""
-filename = "result.png"
+filename = "result"
 images = []
 border = 0
 trim = True
+max_size = 256
 
 try:
     if not os.path.isdir(path):
@@ -96,19 +97,21 @@ def trimmed(img):
                 if pix != 0:
                     return y + 1
 
+    trimRectWidth = get_left()
+    trimRectHeight = get_top()
     cropped = img.crop((get_left(), get_top(), get_right(), get_bot()))
     temp2 = Image.new("RGBA", (cropped.width + border, cropped.height + border))
     temp2.paste(cropped, (0, 0))
-    return temp2
+    return (temp2, trimRectWidth, trimRectHeight)
 
 
 class CanvasNode():
-    def __init__(self, x, y, w, h, image=None):
+    def __init__(self, x, y, w, h, idata=None):
         self.x = x
         self.y = y
         self.width = w
         self.height = h
-        self.image = image
+        self.idata = idata
 
     @property
     def area(self):
@@ -134,12 +137,13 @@ class Canvas():
 
         self.nodes.sort(key=lambda a : a.area)
 
-    def add_image(self, image):
+    def add_image(self, idata):
+        image = idata[0]
         for n in self.nodes:
-            if n.image == None and n.width >= image[0].width and n.height >= image[0].height:
-                newnode = CanvasNode(n.x, n.y, image[0].width, image[0].height, image)
-                left = CanvasNode(n.x + image[0].width, n.y, n.width - image[0].width, image[0].height)
-                bot = CanvasNode(n.x, n.y + image[0].height, n.width, n.height - image[0].height)
+            if n.idata == None and n.width >= image.width and n.height >= image.height:
+                newnode = CanvasNode(n.x, n.y, image.width, image.height, idata)
+                left = CanvasNode(n.x + image.width, n.y, n.width - image.width, image.height)
+                bot = CanvasNode(n.x, n.y + image.height, n.width, n.height - image.height)
                 self.nodes.remove(n) # this should be okay bc returning anyways
                 self.nodes.extend([newnode, left, bot])
                 self.defragment()
@@ -149,16 +153,19 @@ class Canvas():
     def get_result(self):
         result = Image.new("RGBA", (self.width, self.height))
         for n in self.nodes:
-            if n.image != None:
-                result.paste(n.image[0], (n.x, n.y))
+            if n.idata != None:
+                result.paste(n.idata[0], (n.x, n.y))
         return result
 
     def get_json(self):
         result = {}
+        result["METADATA"] = {"name" : filename, "size" : canvas_size}
+        result["sprites"] = []
         for n in self.nodes:
-            if n.image != None:
-                name = n.image[1]
-                result[name] = {"x" : n.x, "y" : n.y, "width" : n.width - border, "height" : n.height - border}
+            if n.idata != None:
+                result["sprites"].append({"name" : n.idata[1],"x" : n.x, \
+                        "y" : n.y, "width" : n.width - border, "height" : n.height - border, \
+                        "trimRect" : {"width" : n.idata[2], "height" : n.idata[3]}})
         return json.dumps(result, indent=4);
 
     def __str__(self):
@@ -168,18 +175,37 @@ class Canvas():
         return res;
 
 
-with os.scandir(path) as it:
-    for entry in it:
-        if entry.is_file:
-            i = Image.open(entry.path)
-            print("Found image:", entry.path)
-            unix_path = entry.path.replace('\\','/')
-            name = unix_path.split('/')[-1].split('.')[0]
-            if trim:
-                images.append((trimmed(i), name))
-            else:
-                images.append((i, name))
+def add_images_from_dir(dir_path):
+    with os.scandir(dir_path) as it:
+        print("Scanning", dir_path)
+        for entry in it:
+            if os.path.isdir(entry.path):
+                add_images_from_dir(entry.path)
+            elif entry.is_file:
+                unix_path = entry.path.replace('\\','/')
+                name = unix_path.split('/')[-1].split('.')[0]
+                try:
+                    i = Image.open(entry.path)
+                    print("Found image:", entry.path)
+                    if trim:
+                        t = trimmed(i)
+                        images.append((t[0], name, t[1], t[2]))
+                    else:
+                        images.append((i, name, 0, 0))
+                except:
+                    print("Ignoring non-image file:", entry.path)
 
+def save_result(canvas, suffix=""):
+    try:
+        canvas.get_result().save(outpath + filename + suffix + ".png")
+        print("Saved sprite sheet at", outpath + filename + suffix  + ".png!")
+        with open(outpath + filename + suffix + ".json", "w") as json_file:
+            json_file.write(canvas.get_json())
+        print("Saved json file at", outpath + filename + suffix + ".json!")
+    except:
+        print("ERROR: Could not save sprite sheet/json! Please ensure all parameters are valid and try again!")
+
+add_images_from_dir(path)
 images.sort(key=lambda i : i[0].height, reverse=True)
 
 canvas_size = 1
@@ -194,13 +220,5 @@ while (not success):
             canvas_size *= 2
             break
     if success:
-        try:
-            canvas.get_result().save(outpath + filename + ".png")
-            print("Saved sprite sheet at", outpath + filename + ".png!")
-            with open(outpath + filename + ".json", "w") as json_file:
-                json_file.write(canvas.get_json())
-            print("Saved json file at", outpath + filename + ".json!")
-            break
-        except:
-            print("ERROR: Could not save sprite sheet/json! Please ensure all parameters are valid and try again!")
-            exit()
+        save_result(canvas)
+        exit()
